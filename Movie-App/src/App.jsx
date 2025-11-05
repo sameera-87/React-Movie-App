24f5506dd9cf7c6449ab2bfa626b685c3e126aca
sameera-87 <sameekumarasinghe87@gmail.com>
2025-11-05 23:35:12 +0530

    movie card basic implementation completed

--- Movie-App/src/App.jsx
+++ Movie-App/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Search from "./components/Search";
 import { Spinner } from "./components/Spinner";
+import MovieCard from "./components/MovieCard";
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -68,7 +69,7 @@ const App = () => {
         </header>
 
         <section className="all-movies">
-          <h2>All Moives</h2>
+          <h2 className="mt-[40px]">All Moives</h2>
 
           {isLoading ? (
             <Spinner />
@@ -77,7 +78,7 @@ const App = () => {
           ) : (
             <ul >
               {movieList.map((movie) => (
-                <p key={movie.id} className="text-white">{movie.title}</p>
+                <MovieCard key={movie.id} movie={movie} />
               ))}
             </ul>
           )}
